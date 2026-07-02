beb5b58c2dfec032776879b1c1b9153c3f0d633a
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2026-07-02 00:14:04 -0300

    (feat) se mejoro la logica de validarPalabra

--- src/services/wordServices.js
+++ src/services/wordServices.js
@@ -5,23 +5,42 @@ export default async function validarPalabra(palabras, palabra) {
     const word = palabra.trim()
     const puntaje = word.length;
 
-    if (palabras.some(e => e.palabra == palabra)) {
-        return [false, undefined, `La palabra ${palabra} ya fue usada`]
-    }
-
     if (word.length >= 36) {
         return [false, undefined, "La palabra excede el maximo permitido"]
     }
 
-    const response = await axios.get(`/api/validate?word=${palabra}`)
+    if (palabras.some(e => e.palabra == word)) {
+        return [false, undefined, `La palabra ${palabra} ya fue usada`]
+    }
 
-    if (response.status == 400) {
-        return [false, undefined, `Palabra invalida "${palabra}"`]
+    if (palabras.length > 0) {
+        const ultimaPalabraValida = palabras[0].palabra;
+
+        const ultimaLetra = ultimaPalabraValida.at(-1).toLowerCase();
+        const primeraLetraNueva = word[0].toLowerCase();
+        if (ultimaLetra !== primeraLetraNueva) {
+            return [
+                false,
+                undefined,
+                `La palabra debe empezar con "${ultimaLetra.toUpperCase()}"`
+            ];
+        }
+    }
+
+    try {
+        const response = await axios.get(`/api/validate?word=${palabra}`)
+
+        return [
+            response.data.exists,
+            { palabra: word, puntos: puntaje },
+            `Palabra valida "${palabra}"`]
+    } catch (error) {
+        if (error.response && error.response.status === 400) {
+            return [false, undefined, `Palabra inválida "${word}"`];
+        }
+
+        return [false, undefined, "Error de comunicación con el servidor"];
     }
 
 
-    return [
-        response.data.exists,
-        { palabra: word, puntos: puntaje },
-        `Palabra valida "${palabra}"`]
 }
