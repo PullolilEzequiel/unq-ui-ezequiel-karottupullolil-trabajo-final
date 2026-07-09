8e01d53e6305957fee59a821667cfe0cd68a69bc
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2026-07-08 23:09:55 -0300

    (feat) Ahora guardarPuntaje solo guarda el puntaje del usuario sí su puntaje es mayor al ultimo puntaje de la lista

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,3 +1,8 @@
+/**
+ * Guarda en el localStorage el puntaje sí es mayor al ultimo puntaje de la lista
+ * @param puntajeTotal puntaje de la partida jugada
+ * @param cantidadDePalabras cantidad de palabras acertadas de la partida jugada
+ */
 export function guardarPuntaje(puntajeTotal, cantidadDePalabras) {
     const nombre = localStorage.getItem("nombre");
 
@@ -6,7 +11,9 @@ export function guardarPuntaje(puntajeTotal, cantidadDePalabras) {
     }
     const puntajesActualesRaw = localStorage.getItem("puntajes");
     const listaPuntajes = puntajesActualesRaw ? JSON.parse(puntajesActualesRaw) : [];
-
+    if(!seTieneQueRegistrar(listaPuntajes, puntajeTotal)){
+        return ;
+    }
     listaPuntajes.push({ nombre, puntajeTotal, cantidadDePalabras })
 
     listaPuntajes.sort((a, b) => b.puntajeTotal - a.puntajeTotal);
@@ -14,6 +21,24 @@ export function guardarPuntaje(puntajeTotal, cantidadDePalabras) {
     const top10Puntajes = listaPuntajes.slice(0, 10);
 
     localStorage.setItem("puntajes", JSON.stringify(top10Puntajes));
+}
+
+
+/**
+ * Indica sí el puntaje obtenido debe ser registrado
+ * @param listaPuntajes el top 10 de puntajes ordenados de mayor a menor
+ * @param puntajeTotal el puntaje a registrar
+ * @returns {boolean}
+ */
+function seTieneQueRegistrar(listaPuntajes, puntajeTotal){
+
+    if(listaPuntajes.length < 10){
+        return true
+    }
+
+    const ultimoPuntajeRegistrado = listaPuntajes[listaPuntajes.length - 1].puntajeTotal;
+
+    return puntajeTotal >= ultimoPuntajeRegistrado;
 }
 
 export function cambiarNombre(nombre) {
